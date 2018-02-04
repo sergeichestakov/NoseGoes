a852d032eb1cf43babee5b91d01675ac1d5d715f
Sergei Chestakov <sergei332@gmail.com>
2018-02-03 17:58:14 -0800

    create class for browser instance

--- controlBrowser.py
+++ controlBrowser.py
@@ -2,15 +2,34 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
-browser = webdriver.Firefox()
-browser.get("https://www.google.com/")
+class Browser:
 
-for tab in range(3):
-    sleep(1)
-    browser.execute_script('''window.open("http://bing.com","_blank");''')
+    default_site = "https://stackoverflow.com/"
+    def __init__(self):
+        self.browser = webdriver.Firefox()
+        self.browser.get(Browser.default_site)
+        #self.populate()
 
-main_window = browser.current_window_handle
+    def populate(self):
+        for tab in range(3):
+            sleep(1)
+            self.browser.execute_script('''window.open("http://bing.com","_blank");''')
 
-sleep(2)
-browser.close()
-browser.quit()
+    def scroll(self):
+        javascript = "window.scroll(0, window.innerHeight);"
+
+        for down in range(10):
+        	self.browser.execute_script(javascript)
+
+
+    def close(self):
+        sleep(5)
+        self.browser.close()
+        self.browser.quit()
+
+
+if __name__ == "__main__":
+    browser = Browser()
+
+    browser.scroll()
+    browser.close()
