6d316f39e49bf16c4e1c5b493a1a047bf2650d99
Sergei Chestakov <sergei332@gmail.com>
2018-02-03 18:09:03 -0800

    page successfully scrolls

--- controlBrowser.py
+++ controlBrowser.py
@@ -4,26 +4,29 @@ from time import sleep
 
 class Browser:
 
+    new_tab = "https://developer.mozilla.org/en-US/"
     default_site = "https://stackoverflow.com/"
     def __init__(self):
         self.browser = webdriver.Firefox()
         self.browser.get(Browser.default_site)
-        #self.populate()
+        self.populate()
 
     def populate(self):
         for tab in range(3):
             sleep(1)
-            self.browser.execute_script('''window.open("http://bing.com","_blank");''')
+            self.browser.execute_script('''window.open("https://developer.mozilla.org/en-US/");''')
 
     def scroll(self):
-        javascript = "window.scroll(0, window.innerHeight);"
+        javascript = "window.scrollBy(0, 500);"
 
         for down in range(10):
-        	self.browser.execute_script(javascript)
+            sleep(1)
+            self.browser.execute_script(javascript)
+            self.browser.execute_script('console.log(window)')
 
 
     def close(self):
-        sleep(5)
+        sleep(10)
         self.browser.close()
         self.browser.quit()
 
