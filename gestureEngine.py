fbaacbe70e095907d411ddd0529bbd9531a6f9e5
Kaelan Mikowicz <eternity01033@gmail.com>
2018-02-03 18:53:24 -0800

    more functions

--- gestureEngine.py
+++ gestureEngine.py
@@ -25,14 +25,7 @@ def drawDebugUI(image, x, y, w, h):
     cv2.circle(image, center=(x, y), radius=2, color=(255,0,0))
     cv2.rectangle(image, (x - w / 2, y - h / 2), (h + w / 2, y + h / 2), color=(0,255,0), thickness=2)
 
-
-def updateGesture(image, face):
-    # face format is [topLeft, topRight, bottomRight, bottomLeft]
-    # each face is a Vertex with properties x and y
-
-    center = ((face[0].x + face[2].x) / 2, (face[0].y + face[2].y) / 2)
-    width = face[2].x - face[0].x
-    height = face[2].y - face[0].y
+def updateSmoothers(center, width, height):
     if "x" not in smoothVar:
         smoothVar["x"] = MovingAverageSmoother(3)
         smoothX = center[0]
@@ -53,4 +46,15 @@ def updateGesture(image, face):
         smoothH = height
     else:
         smoothH = int(smoothVar["h"].smooth(height))
-    drawDebugUI(image, smoothX, smoothY, smoothW, smoothH)
+    
+    return (smoothX, smoothY, smoothW, smoothH)
+
+def updateGesture(image, face):
+    # face format is [topLeft, topRight, bottomRight, bottomLeft]
+    # each face is a Vertex with properties x and y
+
+    center = ((face[0].x + face[2].x) / 2, (face[0].y + face[2].y) / 2)
+    width = face[2].x - face[0].x
+    height = face[2].y - face[0].y
+    (x, y, w, h) = updateSmoothers(center, width, height)
+    drawDebugUI(image, x, y, w, h)
