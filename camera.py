93106cb21bed9a6a155609909de3a6e27a2a3cd6
sergeichestakov <sergei332@gmail.com>
2018-02-03 21:41:56 -0800

    save initial frame for reference

--- camera.py
+++ camera.py
@@ -1,4 +1,3 @@
-# import the necessary packages
 import argparse
 import datetime
 import imutils
@@ -12,26 +11,29 @@ current_time = time.time()
 
 cap = cv2.VideoCapture(0)
 
+#Compare subsequent calculations against the first frame captured
+ret, initFrame = cap.read()
+cv2.imwrite("initialFrame.jpg", initFrame)
+initStats = gc_query.getAnnotations("initialFrame.jpg")[0]
+
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
     delta = time.time() - current_time
     current_time = time.time()
-    # Our operations on the frame come here
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = frame # not really gray here
+
     # Display the resulting frame
     cv2.imwrite("videoframe.jpg", frame)
-    # annotations = gc_query.getAnnotations("videoframe.jpg")[0]
-    # rect = annotations.bounding_poly.vertices
+    #annotations = gc_query.getAnnotations("videoframe.jpg")
+    #rect = annotations.bounding_poly.vertices
     try:
-        rect = tracker.faceDetect(gray)[0]
+        rect = tracker.faceDetect(frame)[0]
         print(rect[0].x)
-        cv2.rectangle(gray, (int(rect[0].x), int(rect[0].y)), (int(rect[2].x), int(rect[2].y)), color=(0,255,0), thickness=2)
+        cv2.rectangle(frame, (int(rect[0].x), int(rect[0].y)), (int(rect[2].x), int(rect[2].y)), color=(0,255,0), thickness=2)
     except Exception:
         print("face not in frame")
 
-    cv2.imshow('frame', gray)
+    cv2.imshow('frame', frame)
     print ("fps: " + str(1/delta))
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
